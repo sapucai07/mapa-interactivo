77582f9300f718f9f6b461b48b363c361c34db69
sapucai07 <samiraceliz07@gmail.com>
2026-04-01 20:54:17 -0300

    Se agrega marcador con popup

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ var map = L.map('map').setView([-34.6037, -58.3816], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: 'OpenStreetMap'
 }).addTo(map);
+// Agregar popup
+L.marker([-34.6037, -58.3816])
+   .addTo(map)
+   .bindPopup("Ubicación inicial")
+   .openPopup();
