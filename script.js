99572d0024cea6b7269379d613331baf2ac6d052
sapucai07 <samiraceliz07@gmail.com>
2026-04-30 21:33:12 -0300

    Ejercicio 2

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
+// 1. Crear mapa
 var map = L.map('map').setView([-34.6037, -58.3816], 13);
 
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   attribution: 'OpenStreetMap'
+// 2. Capas base
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   maxZoom: 19,
+   attribution: '© OpenStreetMap'
 }).addTo(map);
+
+
 // Agregar popup
 L.marker([-34.6037, -58.3816])
    .addTo(map)
    .bindPopup("Ubicación inicial")
    .openPopup();
+
 // clics y coordenadas
-map.on('click', function(e) {
+var popup = L.popup();
+
+map.on('click', function (e) {
    puntos.push([e.latlng.lat, e.latlng.lng]);
 
 
    if (puntos.length === 2) {
-       L.polyline(puntos, { color: 'blue' }).addTo(map);
+      L.polyline(puntos, { color: 'blue' }).addTo(map);
    }
 });
 
 // Dibujar línea entre dos puntos
 let puntos = [];
+// hacer un poligono
+let poligono;
+
+map.on('click', function (e) {
+   puntos.push([e.latlng.lat, e.latlng.lng]);
+})
+poligono = L.polygon(puntos, {
+   color: 'red'
+}).addTo(map); 
