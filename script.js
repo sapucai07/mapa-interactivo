d2b567d94947e9f29624367ff842a625d5dee672
sapucai07 <samiraceliz07@gmail.com>
2026-04-30 21:34:12 -0300

    Se agrega marcador con popup

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// 1. Crear mapa
+// AGREGAR MAPA
 var map = L.map('map').setView([-34.6037, -58.3816], 13);
 
-// 2. Capas base
-var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   maxZoom: 19,
-   attribution: '© OpenStreetMap'
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   attribution: 'OpenStreetMap'
 }).addTo(map);
 
-
-// Agregar popup
+//              MARCADOR CON POPUP
 L.marker([-34.6037, -58.3816])
    .addTo(map)
    .bindPopup("Ubicación inicial")
    .openPopup();
-
-// clics y coordenadas
-var popup = L.popup();
-
-map.on('click', function (e) {
-   puntos.push([e.latlng.lat, e.latlng.lng]);
-
-
-   if (puntos.length === 2) {
-      L.polyline(puntos, { color: 'blue' }).addTo(map);
-   }
-});
-
-// Dibujar línea entre dos puntos
-let puntos = [];
-// hacer un poligono
-let poligono;
-
-map.on('click', function (e) {
-   puntos.push([e.latlng.lat, e.latlng.lng]);
-})
-poligono = L.polygon(puntos, {
-   color: 'red'
-}).addTo(map); 
