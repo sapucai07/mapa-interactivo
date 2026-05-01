3539ded73b597a908d104dc5a125cfd75a45b28b
sapucai07 <samiraceliz07@gmail.com>
2026-05-01 15:17:16 -0300

    Ejercicio 6 Poligono

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 // AGREGAR MAPA
 var map = L.map('map').setView([-34.6037, -58.3816], 13);
 
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: 'OpenStreetMap'
 }).addTo(map);
 
-//              MARCADOR CON POPUP
+// MARCADOR INICIAL
 L.marker([-34.6037, -58.3816])
    .addTo(map)
    .bindPopup("Ubicación inicial")
    .openPopup();
 
-//       modificar evento clic
+// ARRAY DE PUNTOS
+let puntos = [];
+
+// VARIABLE DEL POLIGONO
+let poligono = null;
+
+// EVENTO CLICK
 map.on('click', function(e) {
-   puntos.push([e.latlng.lat, e.latlng.lng]);
+   let lat = e.latlng.lat;
+   let lng = e.latlng.lng;
 
+   puntos.push([lat, lng]);
 
-   if (puntos.length === 2) {
-       L.polyline(puntos, { color: 'blue' }).addTo(map);
+   // si hay 3 puntos dibujar polígono
+   if (puntos.length >= 3) {
+
+       // eliminar el anterior si existe
+       if (poligono) {
+           map.removeLayer(poligono);
+       }
+
+       // crear nuevo polígono
+       poligono = L.polygon(puntos, {
+           color: 'blue',
+           fillColor: 'lightblue',
+           fillOpacity: 0.5
+       }).addTo(map);
    }
 });
 
-
-// Dibujar línea entre dos puntos
-let puntos = [];
