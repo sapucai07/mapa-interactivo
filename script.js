e512b9d31e0d00afa8c1bb3a84cee8827e4967ed
sapucai07 <samiraceliz07@gmail.com>
2026-05-01 15:46:01 -0300

    Ejercicio 7 y 8, Limpiar mapa e Iconos personalizados

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
-// AGREGAR MAPA
+// CREAR MAPA
 var map = L.map('map').setView([-34.6037, -58.3816], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   attribution: 'OpenStreetMap'
+    attribution: 'OpenStreetMap'
 }).addTo(map);
+
+// Marcador personalizado
+var customIcon = L.icon({
+    iconUrl: './img/leaf-green.png',     // tu imagen
+    shadowUrl: './img/leaf-shadow.png',  // opcional
+
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -76]
+});
 
 // MARCADOR INICIAL
 L.marker([-34.6037, -58.3816])
-   .addTo(map)
-   .bindPopup("Ubicación inicial")
-   .openPopup();
+    .addTo(map)
+    .bindPopup("Ubicación inicial")
+    .openPopup();
 
-// ARRAY DE PUNTOS
+// VARIABLES
 let puntos = [];
-
-// VARIABLE DEL POLIGONO
 let poligono = null;
+let marcadores = [];
 
-// EVENTO CLICK
-map.on('click', function(e) {
-   let lat = e.latlng.lat;
-   let lng = e.latlng.lng;
+// CLICK EN EL MAPA
+map.on('click', function (e) {
+    let lat = e.latlng.lat;
+    let lng = e.latlng.lng;
 
-   puntos.push([lat, lng]);
+    puntos.push([lat, lng]);
 
-   // si hay 3 puntos dibujar polígono
-   if (puntos.length >= 3) {
+    // crear marcador
+    let marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
+    marcadores.push(marker);
 
-       // eliminar el anterior si existe
-       if (poligono) {
-           map.removeLayer(poligono);
-       }
+    // dibujar polígono si hay 3 o más puntos
+    if (puntos.length >= 3) {
 
-       // crear nuevo polígono
-       poligono = L.polygon(puntos, {
-           color: 'blue',
-           fillColor: 'lightblue',
-           fillOpacity: 0.5
-       }).addTo(map);
-   }
+        if (poligono) {
+            map.removeLayer(poligono);
+        }
+
+        poligono = L.polygon(puntos, {
+            color: 'blue',
+            fillColor: 'lightblue',
+            fillOpacity: 0.5
+        }).addTo(map);
+    }
 });
 
+// LIMPIAR MAPA
+function limpiarMapa() {
+
+    // borrar marcadores
+    marcadores.forEach(m => map.removeLayer(m));
+    marcadores = [];
+
+    // borrar polígono
+    if (poligono) {
+        map.removeLayer(poligono);
+        poligono = null;
+    }
+
+    // reiniciar puntos
+    puntos = [];
+}
